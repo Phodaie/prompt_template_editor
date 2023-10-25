029002d4595124f10cf91a4b88ba2510dc4f8191
payman hodaie <payman@osellus-mobile.com>
2023-10-25 06:34:41 -0400

    Pointed to AWS Lambda function

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
 import streamlit as st
+import requests
+import json
+from typing import List, Dict
+from pydantic import BaseModel
+
+class Template(BaseModel):
+    template_id: str
+    content: str
+
+
+
+def get_templates()->List[Template]:
+    
+    response = requests.get("https://9v7s2vz54k.execute-api.us-east-1.amazonaws.com/Prod/templates")
+
+    if response.status_code == 200:
+        templates = json.loads(response.content.decode())
+        return [Template(**template) for template in templates]
+    else:
+        return []
+
+
+def update_templates():
+    
+    for template in templates:
+        response = requests.put(f"https://9v7s2vz54k.execute-api.us-east-1.amazonaws.com/Prod/templates/{template.template_id}",data=json.dumps(template.dict()))
+        st.success(json.dumps(template.dict()))
+        if response.status_code == 200:
+            st.success(f"Template {template.template_id} updated")
+        else:
+            st.error(f"Template {template.template_id} failed to update")
+
+templates : List[Template] = []
 
 def main():
     st.subheader("Prompt Template Editor")
     
+    global templates
+    templates = get_templates()
 
+    for template in templates:
 
-
+        with st.expander(template.template_id): 
+            template.content = st.text_area('', template.content , height=200)
+        
+    
+    st.button("Update",on_click=update_templates)
 
 
 
